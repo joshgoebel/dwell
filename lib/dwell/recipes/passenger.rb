737e4fb1a1bd6308c83fada085504f391c04bd35
Robert Bousquet <robert@robertbousquet.com>
2008-11-27 01:16:18 -0800

    Fixed passenger setup, only has most basic vhost config

--- lib/dwell/recipes/passenger.rb
+++ lib/dwell/recipes/passenger.rb
@@ -9,30 +9,30 @@ Capistrano::Configuration.instance(:must_exist).load do
   
       desc "Enable Passenger"
       task :enable_passenger do
-        input = ''
+        input = "\n"
         run "sudo passenger-install-apache2-module" do |ch,stream,out|  
           next if out.chomp == input.chomp || out.chomp == ''  
           print out  
-          ch.send_data(input = $stdin.gets) if out =~ /enter/i  
+          ch.send_data(input) if out =~ /enter/i  
         end
         cfg =<<-EOF
 LoadModule passenger_module /usr/lib/ruby/gems/1.8/gems/passenger-2.0.3/ext/apache2/mod_passenger.so
 PassengerRoot /usr/lib/ruby/gems/1.8/gems/passenger-2.0.3
 PassengerRuby /usr/bin/ruby1.8
         EOF
-        put cfg, "~/passenger"
-        sudo "mv ~/passenger /etc/apache2/conf.d/passenger"
+        put cfg, "/tmp/passenger"
+        sudo "mv /tmp/passenger /etc/apache2/conf.d/passenger"
       end
   
       desc "Setup vhost"
       task :setup_vhost do
         cfg =<<-EOF
 ServerName #{domain}
-ServerAlias #{application}.agilebox.com
+# ServerAlias #{application}.agilebox.com
 DocumentRoot #{deploy_to}/public
         EOF
-        put cfg, "~/vhost"
-        sudo "mv ~/vhost /etc/apache2/sites-available/#{application}"
+        put cfg, "/tmp/vhost"
+        sudo "mv /tmp/vhost /etc/apache2/sites-available/#{application}"
         sudo "a2dissite default"
         sudo "a2ensite #{application}"
       end
@@ -46,7 +46,6 @@ DocumentRoot #{deploy_to}/public
       task :install do
         enable_passenger
         setup_vhost
-        apache_reload
       end
   
     end
