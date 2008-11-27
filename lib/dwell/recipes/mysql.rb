f43006f83932b79feda5e86ac50e5ea0157f9a13
Robert Bousquet <robert@robertbousquet.com>
2008-11-27 00:00:23 -0800

    Bypassed interactive prompts for mysql install

--- lib/dwell/recipes/mysql.rb
+++ lib/dwell/recipes/mysql.rb
@@ -3,8 +3,31 @@ Capistrano::Configuration.instance(:must_exist).load do
     namespace :mysql do
   
       desc "Install MySQL5"
-      task :install do
-        sudo "apt-get install mysql-server mysql-client libmysqlclient15-dev -y"
+      task :install, :roles => :db do
+        sudo "DEBCONF_TERSE='yes' DEBIAN_PRIORITY='critical' DEBIAN_FRONTEND=noninteractive apt-get install -qyu --force-yes mysql-server mysql-client libmysqlclient15-dev"
+      end
+      
+      
+      # Control
+      
+      desc "Start Mysql"
+      task :start, :roles => :db do
+        sudo "/etc/init.d/mysql start"
+      end
+      
+      desc "Stop Mysql"
+      task :stop, :roles => :db do
+        sudo "/etc/init.d/mysql stop"
+      end
+      
+      desc "Restart Mysql"
+      task :restart, :roles => :db do
+        sudo "/etc/init.d/mysql restart"
+      end
+      
+      desc "Reload Mysql"
+      task :reload, :roles => :db do
+        sudo "/etc/init.d/mysql reload"
       end
 
     end
