00bbffced0b1f6550108b7cd37c96b8e96843c7d
Josh Goebel <dreamer3@gmail.com>
2008-12-04 10:14:56 -0500

    sqlite3

--- lib/dwell/recipes/sqlite.rb
+++ lib/dwell/recipes/sqlite.rb
@@ -1,1 +1,14 @@
-## TODO
+Capistrano::Configuration.instance(:must_exist).load do 
+  namespace :dwell do
+    namespace :sqlite do
+  
+      desc "Install Sqlite3"
+      task :install do
+        sudo "apt-get install sqlite3 libsqlite3-dev -y"
+        sudo "gem install sqlite3-ruby --no-rdoc --no-ri"
+        dwell1.record_install "sqlite3"
+      end
+
+    end
+  end
+end
