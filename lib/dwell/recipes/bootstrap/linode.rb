9d9a975ff21aae971e6b832e54c4af47ae92c5d2
Josh Goebel <dreamer3@gmail.com>
2008-12-09 10:59:50 -0500

    ability to set hostname for linode

--- lib/dwell/recipes/bootstrap/linode.rb
+++ lib/dwell/recipes/bootstrap/linode.rb
@@ -19,17 +19,34 @@ Capistrano::Configuration.instance(:must_exist).load do
         sudo "/etc/init.d/ssh reload"
       end
       
-      desc "setup static ip address" 
-      task :setup_static_ip do
-        ip,broadcast,mask,gateway=nil
+      def get_ip_info
         run "/sbin/ifconfig eth0" do |channel, stream, data|
           if data.match(/inet addr:([^ ]*)  Bcast:([^ ]*)  Mask:([^ ]*)/)
-            ip, broadcast, mask = $1, $2, $3
+            return $1, $2, $3
           end
         end
+      end
+      
+      def get_gateway
         run "ip route" do |channel, stream, data|
-          gateway=$1 if data.match(/default via ([^ ]*) dev eth0/)
+          return $1 if data.match(/default via ([^ ]*) dev eth0/)
         end
+      end
+      
+      task :setup_hostname do
+        hostname=ENV["HOSTS"]
+        sudo "echo #{hostname} > /tmp/hostname"
+        sudo "mv /tmp/hostname /etc/hostname"
+        sudo "chown root:root /etc/hostname"
+        host_line="#{get_ip_info.first} #{hostname.split(".").first} #{hostname}"
+        dwell1.append_to_file_if_missing("/etc/hosts",host_line)
+        sudo "hostname --file /etc/hostname"
+      end
+      
+      desc "setup static ip address" 
+      task :setup_static_ip do
+        ip,broadcast,mask=get_ip_info
+        gateway=get_gateway
 
           interface=<<-EOS
 iface eth0 inet static
@@ -47,6 +64,15 @@ gateway #{gateway}
       
       desc "bootstrap linode box"
       task :bootstrap do
+        hostname=ENV["HOSTS"]
+        if hostname =~ /\d+\.\d+\.\d+\.\d+/
+          puts "You used an IP, please use the hostname so can set /etc/hostname on the remote."
+          exit
+        end
+        if hostname.nil? or hostname.empty?
+          puts "You must specify HOSTS='hostname' to use the bootstrap command."
+          exit
+        end
         set :deploy_user, user
         auth_keys="config/dwell/authorized_keys/#{deploy_user}"
         unless File.exist?(auth_keys)
@@ -67,6 +93,7 @@ gateway #{gateway}
         # test deploy login via ssh before we disable root login
         sudo "echo"
         disable_root_login
+        setup_hostname
         setup_static_ip
       end
 
