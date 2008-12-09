3196100554de1dab8f84adb1440748feb03f258c
Josh Goebel <dreamer3@gmail.com>
2008-12-09 16:07:17 -0500

    push ssh keys should remove old authorizations and only use those you have locally

--- lib/dwell/recipes/bootstrap/server.rb
+++ lib/dwell/recipes/bootstrap/server.rb
@@ -2,6 +2,8 @@ Capistrano::Configuration.instance(:must_exist).load do
   namespace :dwell do
     namespace :server do
 
+      set :deploy_user, nil
+
       task :create_deploy_user do
         dwell1.adduser deploy_user, :group => "admin"
         puts
@@ -9,14 +11,18 @@ Capistrano::Configuration.instance(:must_exist).load do
         dwell1.invoke_with_input("passwd #{deploy_user}", /UNIX password/, new_password)
       end
       
-      task :copy_ssh_keys do
-        dwell1.mkdir "/home/#{deploy_user}/.ssh", :mode => 0700, :owner => "#{deploy_user}.admin"
+      task :push_ssh_keys do
+        set :deploy_user, user if deploy_user.nil?
+        puts "pushing keys for: #{deploy_user}"
+        dwell1.mkdir "/home/#{deploy_user}/.ssh", :mode => 0700, 
+          :owner => "#{deploy_user}.admin", :via => :sudo
+        run "rm -f /home/#{deploy_user}/.ssh/authorized_keys"
         Dir.glob("config/dwell/authorized_keys/#{deploy_user}/*").each do |file|
           key=File.read(file)
           dwell1.append_to_file_if_missing("/home/#{deploy_user}/.ssh/authorized_keys", key)
         end
         sudo "chown #{deploy_user}.admin /home/#{deploy_user}/.ssh/authorized_keys"
-        sudo "chmod 0600 /home/#{deploy_user}/.ssh/authorized_keys"
+        run "chmod 0600 /home/#{deploy_user}/.ssh/authorized_keys"
       end
       
       task :disable_root_login do
@@ -42,7 +48,7 @@ Capistrano::Configuration.instance(:must_exist).load do
           sudo "apt-get install ubuntu-standard -y" 
         end
         create_deploy_user
-        copy_ssh_keys
+        push_ssh_keys
         set :user, deploy_user        
         # test deploy login via ssh before we disable root login
         sudo "echo"
